2be42b9651d1044a875be9f76f1b696ae8bd476d
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2022-02-12 11:48:49 -0300

    add tests validate CPF

--- aula_1/example2/example2.test.js
+++ aula_1/example2/example2.test.js
@@ -3,41 +3,53 @@ const example2 = require("./example2");
 test("Verificar se CPF é nulo", function () {
 	const cpf = null;
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
 test("Verificar se CPF é undefinied", function () {
 	const cpf = undefined;
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
 test("Verificar se CPF é branco", function () {
 	const cpf = '';
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
 test("Verificar se CPF possui a quantidade correta de números", function () {
 	const cpf = "95.411.347-80";
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
 test("Verificar se todos os numeros do CPF são iguais", function () {
 	const cpf = "111.111.111-11";
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
 test("Verificar se CPF possui digito verificador inválido", function () {
 	const cpf = '123.456.789-99';
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(false);
+	expect(resultValidade).toBeFalsy();
 });
 
-test("Verificar se CPF possui digito verificador valido", function () {
+test("Verificar se CPF é valido A", function () {
 	const cpf = "935.411.347-80";
 	const resultValidade = example2.validateCPF(cpf);
-	expect(resultValidade).toBe(true);
+	expect(resultValidade).toBeTruthy();
+});
+
+test("Verificar se CPF é valido B", function () {
+	const cpf = "357.188.378-05";
+	const resultValidade = example2.validateCPF(cpf);
+	expect(resultValidade).toBeTruthy();
+});
+
+test("Verificar se CPF é valido C", function () {
+	const cpf = "987.654.321-00";
+	const resultValidade = example2.validateCPF(cpf);
+	expect(resultValidade).toBeTruthy();
 });
