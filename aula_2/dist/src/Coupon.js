d1836c68a08e29629bada8cd626b4baa6971b648
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2022-02-17 23:42:18 -0300

    remove dist aula_2

--- aula_2/dist/src/Coupon.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Coupon {
-    constructor(code, percentage) {
-        this.code = code;
-        this.percentage = percentage;
-    }
-}
-exports.default = Coupon;
